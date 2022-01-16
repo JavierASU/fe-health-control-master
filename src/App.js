2a6986aa3c015cba7aebdf75c984c4d69d970a42
juancjr <juancarlo14071@gmail.com>
2022-01-16 20:58:35 -0300

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       
-      <Container>
+      <Container >
         <Routes>
           <Route path="/NewPaciente" element={<NewPaciente />} />
           <Route path="/Pacientes" element={<Pacientes />} />
