0b216662d9ec8aa6d2d5a2c71050b16ab9b43144
Javier Soto <s_javier_11@hotmail.com>
2022-01-16 20:01:36 -0400

    Update css2

--- src/Start/Welcome.js
+++ src/Start/Welcome.js
@@ -13,12 +13,12 @@ export default function Welcome() {
     const navigate = useNavigate()
     return (
         <div >
-            <Container className="mydiv animate-bg fondo1">
+            <Container >
                 <Grid container alignItems="center" justifyItems="center" direction="column" sx={{ mt: 10 }}
                 >
                     <Typography color={"white"} className='span' variant='h4'>Bienvenido a Mascotas Del centro</Typography>
-                    <img src={IMG1} className='img1' />
-                    <ButtonBase  onClick={() => navigate("/Pacientes")}><Typography variant='h5' color={"white"}>Continuar</Typography><ArrowForwardIosIcon color={"white"}></ArrowForwardIosIcon></ButtonBase>
+                    <img src={IMG4} className='img1' />
+                    <ButtonBase className='button'  onClick={() => navigate("/Pacientes")}><Typography variant='h5' color={"white"}>Continuar</Typography><ArrowForwardIosIcon color={"white"}></ArrowForwardIosIcon></ButtonBase>
                 </Grid>
             </Container>
             <div>
