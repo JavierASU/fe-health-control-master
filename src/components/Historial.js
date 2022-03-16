13fbc222572adf0a814cce145feb2f732aabef62
juancjr <juancarlo14071@gmail.com>
2022-03-16 20:41:08 -0300

    update

--- src/components/Historial.js
+++ src/components/Historial.js
@@ -1,19 +1,5 @@
-<<<<<<< HEAD
-import {
-  Button,
-  Card,
-  CardContent,
-  FormControl,
-  Grid,
-  Modal,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { useState } from "react";
-=======
 import { Button, Card, CardContent, FormControl, Grid, Modal, TextField, Typography, TextareaAutosize } from '@mui/material'
 import { useState } from 'react'
->>>>>>> afcdeade58cea0f9d87447005fc6d0e7e04bd3b7
 import axios from "axios";
 import DoneIcon from "@mui/icons-material/Done";
 import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
@@ -55,11 +41,6 @@ export const Historial = (props) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-<<<<<<< HEAD
-    const nw = {
-      pacienteId: id,
-      informe: paciente.informe,
-=======
         id,
         cedula,
         nombreTitular,
@@ -103,7 +84,6 @@ export const Historial = (props) => {
         );
         console.log(res)
         window.location.href = "/Pacientes";
->>>>>>> afcdeade58cea0f9d87447005fc6d0e7e04bd3b7
     };
     const res = await axios.post(
       "http://localhost:8081/api/health-control/v1/historiales",
@@ -113,16 +93,6 @@ export const Historial = (props) => {
     window.location.href = "/Pacientes";
   };
 
-<<<<<<< HEAD
-  const handleChange = (e, valueName, setPaciente) => {
-    let value = e.target.value;
-    setPaciente((paciente) => ({
-      ...paciente,
-      [valueName]: value,
-    }));
-  };
-  console.log(paciente);
-=======
     const handleChange = (e, valueName, setPaciente) => {
         let value = e.target.value;
         setPaciente((paciente) => ({
@@ -131,7 +101,6 @@ export const Historial = (props) => {
         }));
     };
     
->>>>>>> afcdeade58cea0f9d87447005fc6d0e7e04bd3b7
 
   return (
     <div>
@@ -153,50 +122,6 @@ export const Historial = (props) => {
                 padding: "1rem",
               }}
             >
-<<<<<<< HEAD
-              <Typography variant="h4" textAlign="center" color="white">
-                Crear Historial
-              </Typography>
-              <Typography color={"white"} textAlign="center">
-                Titular:{nombreMascota}
-              </Typography>
-              <Typography color={"white"} textAlign="center">
-                Propietario:{nombreTitular}
-              </Typography>
-              <CardContent>
-                <div>
-                  <FormControl style={{ overflow: "scroll" }}>
-                    <TextField
-                      required={true}
-                      name="informe"
-                      onChange={(e) => handleChange(e, "informe", setPaciente)}
-                      value={paciente.informe}
-                      variant="outlined"
-                      label="Agregar Historial"
-                      multiline
-                      sx={{ m: 1, mt: 3, width: "100ch" }}
-                      name="comentarios"
-                      inputProps={{ style: { color: "white" } }}
-                      InputLabelProps={{ style: { color: "white" } }}
-                    ></TextField>
-                    <Button
-                      color="success"
-                      variant="contained"
-                      onClick={handleSubmit}
-                    >
-                      <DoneIcon></DoneIcon>
-                    </Button>
-                  </FormControl>
-                </div>
-              </CardContent>
-            </Card>
-          </Grid>
-        </Grid>
-      </Modal>
-    </div>
-  );
-};
-=======
                 <Grid container direction="column" alignItems="center" justifyContent="center">
                     <Grid item xs={3}>
                         <Card sx={{ mt: 5 }}
@@ -237,6 +162,5 @@ export const Historial = (props) => {
         </div>
     )
 }
->>>>>>> afcdeade58cea0f9d87447005fc6d0e7e04bd3b7
 
 export default Historial;
